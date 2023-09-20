abeb5edf86d8e258dd0f3b8a9dd5c8ab91b38897
atlas <98959780+PotatoArtie@users.noreply.github.com>
2023-09-21 01:48:57 +0900

    [add] another way to use of scene phase

--- Labs/Playground/LosingWeight/LosingWeight Watch App/ContentView.swift
+++ Labs/Playground/LosingWeight/LosingWeight Watch App/ContentView.swift
@@ -10,20 +10,47 @@ import HealthKit
 
 struct ContentView: View {
     @Environment(\.scenePhase) private var scenePhase
+    @State var isInactivated = true
     
     var body: some View {
-        if scenePhase == .active {
-            VStack {
+        VStack{
+            
+            // Way 1
+            if isInactivated {
+                Image(systemName: "figure.walk.motion")
+                    .imageScale(.large)
+                    .padding()
+                    .onChange(of: scenePhase) { oldValue, newValue in
+                        isInactivated = newValue == .inactive
+                    }
+            } else {
                 Text("Hello, world!")
                 Image(systemName: "figure.walk")
                     .imageScale(.large)
                     .padding()
+                    .onChange(of: scenePhase) { oldValue, newValue in
+                        isInactivated = newValue == .inactive
+                    }
             }
-            .padding()
-        } else {
-            Image(systemName: "figure.walk.motion")
-                .imageScale(.large)
+            
+            
+            //Way 2
+            /*
+            if scenePhase == .active {
+                VStack {
+                    Text("Hello, world!")
+                    Image(systemName: "figure.walk")
+                        .imageScale(.large)
+                        .padding()
+                }
                 .padding()
+            } else {
+                Image(systemName: "figure.walk.motion")
+                    .imageScale(.large)
+                    .padding()
+            }
+             */
+            
         }
     }
 }
